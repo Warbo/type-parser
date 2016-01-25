1290b27b3a1d6be1c6e60ac1408a916723410351
Chris Warburton <chriswarbo@gmail.com>
2016-01-25 13:35:28 +0000

    Focus on extracting argument types

--- src/Parse.hs
+++ src/Parse.hs
@@ -5,19 +5,54 @@ import qualified Language.Haskell.Exts.Parser as Parser
 import qualified Language.Haskell.Exts.Pretty as Pretty
 import           Language.Haskell.Exts.Syntax
 
-data RoseTree = Leaf String | Node [RoseTree] deriving (Eq)
+-- | Parses a String as a type, returning component parts of any function
+--   argument types. For example, given "(Int, Char) -> Bool -> Float" will
+--   return a list like [(Int, Char), Bool, Int, Char]. Given, e.g. "Int", will
+--   return []
+parseArgs :: String -> [Type]
+parseArgs s = case parseType s of
+  Just t  -> argTypes t
+  Nothing -> []
 
-instance Show RoseTree where
-  show (Leaf x) = show x
-  show (Node xs) = "(" ++ intercalate " " (map show xs) ++ ")"
-
-parseType :: String -> Maybe RoseTree
+parseType :: String -> Maybe Type
 parseType x = case Parser.parseType x of
   Parser.ParseFailed _ _ -> Nothing
-  Parser.ParseOk t       -> Just (parseType' t)
+  Parser.ParseOk t       -> Just t
 
-parseType' :: Type -> RoseTree
-parseType' t = case t of
-  TyCon name -> Leaf (pp name)
+-- | Get all components of the argument parts of a type, e.g. for
+--   Int -> [Bool] -> String will return a list like [Int, [Bool], Bool]
+argTypes :: Type -> [Type]
+argTypes t = if "->" `isInfixOf` (pp t)
+                then let topArgs = init (splitType t)
+                      in topArgs ++ concatMap bitsOf topArgs
+                else []
 
-pp = Pretty.prettyPrint
+-- | Split up a function type Foo -> Bar -> Baz into its components
+--   [Foo, Bar, Baz]
+splitType (TyForall _ _ x) = splitType x
+splitType (TyFun x y)      = x : splitType y
+splitType t                = [t]
+
+-- | Recursively break apart a type, to get all of the components it's made of,
+--   e.g. for "Either [Bool] Int" will return a list like [Either [Bool] Int,
+--   [Bool], Int, Bool]
+bitsOf :: Type -> [Type]
+bitsOf t = t : case t of
+  TyForall   _ _ x -> bitsOf x
+  TyList     x     -> bitsOf x
+  TyParArray x     -> bitsOf x
+  TyParen    x     -> bitsOf x
+  TyKind     x _   -> bitsOf x
+  TyBang     _ x   -> bitsOf x
+  TyFun      x y   -> bitsOf x ++ bitsOf y
+  TyApp      x y   -> bitsOf x ++ bitsOf y
+  TyInfix    x _ y -> bitsOf x ++ bitsOf y
+  TyEquals   x y   -> bitsOf x ++ bitsOf y
+  TyTuple    _ xs  -> concatMap bitsOf xs
+  TyPromoted x     -> []
+  _                -> []
+
+pp = Pretty.prettyPrintStyleMode (Pretty.style {
+                                     Pretty.mode = Pretty.OneLineMode
+                                  })
+                                 Pretty.defaultMode
--- test/Main.hs
+++ test/Main.hs
@@ -1,20 +1,66 @@
 module Main where
 
+import           Data.List
+import           Data.Maybe
 import           Parse
 import           Test.QuickCheck
 import           Test.Tasty             (defaultMain, testGroup, localOption)
 import           Test.Tasty.QuickCheck
 
 main = defaultMain $ testGroup "All tests" [
-    testProperty "Simple types"  canReadSimpleTypes
-  , testProperty "Applied types" canReadAppliedTypes
+    testProperty "Simple types have no args"         simpleTypesNoArgs
+  , testProperty "Simple functions have simple args" simpleFunctionArgs
+  , testProperty "Can split up function types"       canSplitFunTypes
+  --, testProperty "Applied types" canReadAppliedTypes
   ]
 
-canReadSimpleTypes (TyCon s) = parseType s === Just (Leaf s)
+simpleTypesNoArgs t = parseArgs (type2String (TC t)) === []
 
-canReadAppliedTypes = False
+simpleFunctionArgs (Positive n) (NonEmpty ts) =
+    counterexample (show (("funType", funType),
+                          ("strBits", show strBits),
+                          ("found",   show found)))
+                   test
+  where bits    = take (n+1) (cycle ts)
+        strBits = map (type2String . TC) bits
+        funType = intercalate " -> " strBits
+        expect  = init strBits
+        found   = map pp (parseArgs funType)
+        gotExpected  = all (`elem` found)  expect
+        noUnexpected = all (`elem` expect) found
+        test = gotExpected && noUnexpected
+
+canSplitFunTypes (NonEmpty ts) =
+    counterexample (show (("bits",   show bits),
+                          ("funT",   show funT),
+                          ("foundS", show foundS)))
+                   test
+  where bits = map type2String ts
+        funT = intercalate " -> " bits
+        parsed = fromJust (parseType funT)
+        found  = splitType parsed
+        foundS = map pp found
+        gotExpected  = all (`elem` bits)   foundS
+        noUnexpected = all (`elem` foundS) bits
+        test         = gotExpected && noUnexpected
+
+--canReadAppliedTypes t@(TyApp ts) =
+--  stringThenParse (TA t) === Just (Node (mapMaybe (stringThenParse) ts))
+
+stringThenParse = argsAsStrings . type2String
+
+argsAsStrings = map pp . parseArgs
+
+type2String :: Type -> String
+type2String (TC (TyCon t))  = t
+type2String (TA (TyApp ts)) = "((" ++ intercalate ") ("    (map type2String ts) ++ "))"
+type2String (TF (TyFun ts)) = "((" ++ intercalate ") -> (" (map type2String ts) ++ "))"
 
 newtype TyCon = TyCon String deriving (Show)
+newtype TyApp = TyApp [Type] deriving (Show)
+newtype TyFun = TyFun [Type] deriving (Show)
+
+data Type = TC TyCon | TA TyApp | TF TyFun deriving (Show)
 
 instance Arbitrary TyCon where
   arbitrary = do initial <- elements upper
@@ -24,3 +70,26 @@ instance Arbitrary TyCon where
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower = "abcdefghijklmnopqrstuvwxyz"
 alpha = upper ++ lower
+
+sizedList g 0 = do x <- g 0
+                   return [x]
+sizedList g n = do i  <- choose (1,n)
+                   x  <- g i
+                   xs <- sizedList g (n-i)
+                   return (x:xs)
+
+sizedTyApp :: Int -> Gen TyApp
+sizedTyApp n = TyApp <$> sizedList sizedType n
+
+sizedTyFun n = TyFun <$> sizedList sizedType n
+
+sizedType 0 = TC <$> arbitrary
+sizedType n = oneof [TC <$> arbitrary, TA <$> sizedTyApp n, TF <$> sizedTyFun n]
+
+instance Arbitrary TyApp where
+  arbitrary = do n <- choose (0, 100)
+                 sizedTyApp n
+
+instance Arbitrary Type where
+  arbitrary = do n <- choose (0, 100)
+                 sizedType n
